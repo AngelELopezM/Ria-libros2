fae3701c2ce77131e04505a357b84e38f1b56fbb
Angel Lopez <enmanuelm15@hotmail.com>
2021-09-03 14:33:44 -0400

    Hasta aqui se puede agregar un pdf con el libro que se quiere subir, tambien se valida que el libro no exista utilizando el nombre de libro dijitado y el autor del mismo

--- ria libros/wwwroot/js/site.js
+++ ria libros/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
 
-// Write your JavaScript code.
+function alert(message, type) {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+    alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', function () {
+        alert('Nice, you triggered this alert message!', 'success')
+    })
+}
